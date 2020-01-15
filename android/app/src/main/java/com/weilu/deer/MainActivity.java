356b4f63d04eb818359c34420693d84b55a7701c
weilu <a05111993@163.com>
2020-01-15 10:20:02 +0800

    image_picker 支持v2

--- android/app/src/main/java/com/weilu/deer/MainActivity.java
+++ android/app/src/main/java/com/weilu/deer/MainActivity.java
@@ -13,9 +13,5 @@ public class MainActivity extends FlutterActivity {
   @Override
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
     flutterEngine.getPlugins().add(new InstallAPKPlugin(this));
-    // https://github.com/flutter/flutter/issues/47021
-    // ImagePickerPlugin暂不支持v2插件api，导致初始化未能完成。这里手动初始化一遍。
-    ShimPluginRegistry shimPluginRegistry = new ShimPluginRegistry(flutterEngine);
-    io.flutter.plugins.imagepicker.ImagePickerPlugin.registerWith(shimPluginRegistry.registrarFor("io.flutter.plugins.imagepicker.ImagePickerPlugin"));
   }
 }
