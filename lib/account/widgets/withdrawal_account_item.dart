ffe5ac5fa17179718663de6e172669a0adbe44ef
weilu <a05111993@163.com>
2020-04-28 13:00:56 +0800

    动画优化

--- lib/account/widgets/withdrawal_account_item.dart
+++ lib/account/widgets/withdrawal_account_item.dart
@@ -35,9 +35,6 @@ class _WithdrawalAccountItemState extends State<WithdrawalAccountItem> with Sing
     super.initState();
     _animationController = AnimationController(vsync: this, duration: Duration(seconds: 1));
     _animation = Tween(end: 1.0, begin: 0).animate(_animationController)
-      ..addListener(() {
-        setState(() {});
-      })
       ..addStatusListener((status) {
         _animationStatus = status;
       });
@@ -45,81 +42,89 @@ class _WithdrawalAccountItemState extends State<WithdrawalAccountItem> with Sing
   
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 15.0, left: 22.0, right: 22.0),
-      child: Transform(
-        alignment: FractionalOffset.center,
-        transform: Matrix4.identity()
-          ..setEntry(3, 2, 0.002)
-          ..rotateX(pi * _animation.value),
-        child: AccountCard(
-          type: widget.data.type,
-          child: InkWell(
-            // 长按删除账号
-            onLongPress: () => widget.onLongPress(),
-            onTap: () {
-              if (_animationStatus == AnimationStatus.dismissed) {
-                _animationController.forward();
-              } else {
-                _animationController.reverse();
-              }
-            },
-            child: _animation.value <= 0.5 ? Stack(
-              children: <Widget>[
-                Positioned(
-                  top: 25.0,
-                  left: 24.0,
-                  child: Container(
-                      height: 40.0,
-                      width: 40.0,
-                      padding: const EdgeInsets.all(8.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                      child: LoadAssetImage(widget.data.type == 1 ? 'account/wechat' : 'account/yhk')
-                  ),
-                ),
-                Positioned(
-                  top: 22.0,
-                  left: 72.0,
-                  child: Text(widget.data.typeName, style: TextStyle(color: Colors.white, fontSize: Dimens.font_sp18)),
-                ),
-                Positioned(
-                  top: 48.0,
-                  left: 72.0,
-                  child: Text(widget.data.name, style: TextStyle(color: Colors.white, fontSize: Dimens.font_sp12)),
-                ),
-                Positioned(
-                  bottom: 24.0,
-                  left: 72.0,
-                  child: Text(widget.data.code, style: TextStyle(color: Colors.white, fontSize: Dimens.font_sp18, letterSpacing: 1.0)),
-                ),
-              ],
-            ) : Center(
-              child: GestureDetector(
-                onTap: () => Toast.show('提现'),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 5.0),
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 0.6, color: Colors.white),
-                    borderRadius: BorderRadius.circular(4.0),
-                  ),
-                  child: Transform(
-                    // 文字翻转，保证文字的方向
-                    alignment: FractionalOffset.center,
-                    transform: Matrix4.identity()
-                      ..setEntry(3, 2, 0.002)
-                      ..rotateX(pi),
-                    child: Text('提现',
-                      style: TextStyle(color: Colors.white, fontSize: Dimens.font_sp16)
-                    ),
-                  ),
-                ),
+    var front = Stack(
+      children: <Widget>[
+        Positioned(
+          top: 25.0,
+          left: 24.0,
+          child: Container(
+              height: 40.0,
+              width: 40.0,
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20.0),
               ),
+              child: LoadAssetImage(widget.data.type == 1 ? 'account/wechat' : 'account/yhk')
+          ),
+        ),
+        Positioned(
+          top: 22.0,
+          left: 72.0,
+          child: Text(widget.data.typeName, style: TextStyle(color: Colors.white, fontSize: Dimens.font_sp18)),
+        ),
+        Positioned(
+          top: 48.0,
+          left: 72.0,
+          child: Text(widget.data.name, style: TextStyle(color: Colors.white, fontSize: Dimens.font_sp12)),
+        ),
+        Positioned(
+          bottom: 24.0,
+          left: 72.0,
+          child: Text(widget.data.code, style: TextStyle(color: Colors.white, fontSize: Dimens.font_sp18, letterSpacing: 1.0)),
+        ),
+      ],
+    );
+    
+    var back = Center(
+      child: GestureDetector(
+        onTap: () => Toast.show('提现'),
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 5.0),
+          decoration: BoxDecoration(
+            border: Border.all(width: 0.6, color: Colors.white),
+            borderRadius: BorderRadius.circular(4.0),
+          ),
+          child: Transform(
+            // 文字翻转，保证文字的方向
+            alignment: FractionalOffset.center,
+            transform: Matrix4.identity()
+              ..setEntry(3, 2, 0.002)
+              ..rotateX(pi),
+            child: Text('提现',
+                style: TextStyle(color: Colors.white, fontSize: Dimens.font_sp16)
             ),
           ),
         ),
+      ),
+    );
+    return Padding(
+      padding: const EdgeInsets.only(top: 15.0, left: 22.0, right: 22.0),
+      child: AnimatedBuilder(
+        animation: _animation,
+        builder: (_, child) {
+          return Transform(
+            alignment: FractionalOffset.center,
+            transform: Matrix4.identity()
+              ..setEntry(3, 2, 0.002)
+              ..rotateX(pi * _animation.value),
+            child: AccountCard(
+              type: widget.data.type,
+              child: InkWell(
+                // 长按删除账号
+                onLongPress: () => widget.onLongPress(),
+                onTap: () {
+                  if (_animationStatus == AnimationStatus.dismissed) {
+                    _animationController.forward();
+                  } else {
+                    _animationController.reverse();
+                  }
+                },
+                child: _animation.value <= 0.5 ?  front : back,
+              ),
+            ),
+          );
+        },
       ),
     );
   }
--- lib/goods/widgets/goods_item.dart
+++ lib/goods/widgets/goods_item.dart
@@ -37,181 +37,201 @@ class GoodsItem extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+    var child = Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        ExcludeSemantics(child: LoadImage(item.icon, width: 72.0, height: 72.0)),
+        Gaps.hGap8,
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text(
+                  '八月十五中秋月饼礼盒',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis
+              ),
+              Gaps.vGap4,
+              Row(
+                children: <Widget>[
+                  Visibility(
+                    // 默认为占位替换，类似于gone
+                    visible: item.type % 3 == 0,
+                    child: _GoodsItemTag(
+                      text: '立减',
+                      color: Theme.of(context).errorColor,
+                    ),
+                  ),
+                  Opacity(
+                    // 修改透明度实现隐藏，类似于invisible
+                    opacity: item.type % 2 != 0 ? 0.0 : 1.0,
+                    child: _GoodsItemTag(
+                      text: '社区币抵扣',
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  )
+                ],
+              ),
+              Gaps.vGap16,
+              Text(Utils.formatPrice('20.00', format: MoneyFormat.NORMAL))
+            ],
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            Semantics(
+              /// container属性为true，防止上方ExcludeSemantics去除此处语义
+              container: true,
+              label: '商品操作菜单',
+              child: GestureDetector(
+                child: Container(
+                    key: Key('goods_menu_item_$index'),
+                    width: 44.0,
+                    height: 44.0,
+                    color: Colors.transparent,
+                    padding: const EdgeInsets.only(left: 28.0, bottom: 28.0),
+                    child: const LoadAssetImage('goods/ellipsis')
+                ),
+                onTap: onTapMenu,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: Text(
+                '特产美味',
+                style: Theme.of(context).textTheme.subtitle,
+              ),
+            )
+          ],
+        )
+      ],
+    );
+    
     return Stack(
       children: <Widget>[
+        // item间的分隔线
         Container(
           padding: const EdgeInsets.only(left: 16.0, top: 16.0),
           child: DecoratedBox(
             decoration: BoxDecoration(
-                border: Border(
-                  bottom: Divider.createBorderSide(context, width: 0.8),
-                )
+              border: Border(
+                bottom: Divider.createBorderSide(context, width: 0.8),
+              )
             ),
             child: Padding(
               padding: const EdgeInsets.only(right: 16.0, bottom: 16.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  ExcludeSemantics(child: LoadImage(item.icon, width: 72.0, height: 72.0)),
-                  Gaps.hGap8,
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        const Text(
-                          '八月十五中秋月饼礼盒',
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis
-                        ),
-                        Gaps.vGap4,
-                        Row(
-                          children: <Widget>[
-                            Visibility(
-                              // 默认为占位替换，类似于gone
-                              visible: item.type % 3 == 0,
-                              child: Container(
-                                padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                                margin: const EdgeInsets.only(right: 4.0),
-                                decoration: BoxDecoration(
-                                  color: Theme.of(context).errorColor,
-                                  borderRadius: BorderRadius.circular(2.0),
-                                ),
-                                height: 16.0,
-                                alignment: Alignment.center,
-                                child: const Text(
-                                  '立减',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: Dimens.font_sp10
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Opacity(
-                              // 修改透明度实现隐藏，类似于invisible
-                              opacity: item.type % 2 != 0 ? 0.0 : 1.0,
-                              child: Container(
-                                padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                                decoration: BoxDecoration(
-                                  color: Theme.of(context).primaryColor,
-                                  borderRadius: BorderRadius.circular(2.0),
-                                ),
-                                height: 16.0,
-                                alignment: Alignment.center,
-                                child: const Text(
-                                  '社区币抵扣',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: Dimens.font_sp10
-                                  ),
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                        Gaps.vGap16,
-                        Text(Utils.formatPrice('20.00', format: MoneyFormat.NORMAL))
-                      ],
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      Semantics(
-                        /// container属性为true，防止上方ExcludeSemantics去除此处语义
-                        container: true,
-                        label: '商品操作菜单',
-                        child: GestureDetector(
-                          child: Container(
-                            key: Key('goods_menu_item_$index'),
-                            width: 44.0,
-                            height: 44.0,
-                            color: Colors.transparent,
-                            padding: const EdgeInsets.only(left: 28.0, bottom: 28.0),
-                            child: const LoadAssetImage('goods/ellipsis')
-                          ),
-                          onTap: onTapMenu,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 10.0),
-                        child: Text(
-                          '特产美味',
-                          style: Theme.of(context).textTheme.subtitle,
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
+              child: child
             ),
           ),
         ),
-        selectIndex != index ? Gaps.empty : Positioned.fill(
-          child: AnimatedBuilder(
-              animation: animation,
-              builder:(_, child) {
-                bool isDark = ThemeUtils.isDark(context);
-                Color buttonColor = isDark ? Colours.dark_text : Colors.white;
-                return MenuReveal(
-                  revealPercent: animation.value,
-                  child: InkWell(
-                    onTap: onTapMenuClose,
-                    child: Container(
-                      color: isDark ? const Color(0xB34D4D4D) : const Color(0x4D000000),
-                      child: Theme( // 修改button默认的最小宽度与padding
-                        data: Theme.of(context).copyWith(
-                          buttonTheme: ButtonThemeData(
-                            padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                            minWidth: 56.0,
-                            height: 56.0,
-                            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap, // 距顶部距离为0
-                            shape:RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(24.0),
-                            ),
-                          ),
-                          textTheme: TextTheme(
-                            button: TextStyle(
-                              fontSize: Dimens.font_sp16,
-                            )
-                          ),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Gaps.hGap15,
-                            FlatButton(
-                              key: Key('goods_edit_item_$index'),
-                              textColor: isDark ?  Colours.dark_button_text : Colors.white,
-                              color: isDark ?  Colours.dark_app_main : Colours.app_main,
-                              child: const Text('编辑'),
-                              onPressed: onTapEdit,
-                            ),
-                            FlatButton(
-                              key: Key('goods_operation_item_$index'),
-                              textColor: Colours.text,
-                              color: buttonColor,
-                              child: const Text('下架'),
-                              onPressed: onTapOperation,
-                            ),
-                            FlatButton(
-                              key: Key('goods_delete_item_$index'),
-                              textColor: Colours.text,
-                              color: buttonColor,
-                              child: const Text('删除'),
-                              onPressed: onTapDelete,
-                            ),
-                            Gaps.hGap15,
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                );
-              }
-          ),
-        )
+        selectIndex != index ? Gaps.empty : _buildGoodsMenu(context),
       ],
+    );
+  }
+  
+  _buildGoodsMenu(BuildContext context) {
+    return Positioned.fill(
+      child: AnimatedBuilder(
+        animation: animation,
+        child: _buildGoodsMenuWidget(context),
+        builder:(_, child) {
+          return MenuReveal(
+            revealPercent: animation.value,
+            child: child
+          );
+        }
+      ),
+    );
+  }
+
+  _buildGoodsMenuWidget(BuildContext context) {
+    bool isDark = ThemeUtils.isDark(context);
+    Color buttonColor = isDark ? Colours.dark_text : Colors.white;
+    return InkWell(
+      onTap: onTapMenuClose,
+      child: Container(
+        color: isDark ? const Color(0xB34D4D4D) : const Color(0x4D000000),
+        child: Theme( // 修改button默认的最小宽度与padding
+          data: Theme.of(context).copyWith(
+            buttonTheme: ButtonThemeData(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              minWidth: 56.0,
+              height: 56.0,
+              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap, // 距顶部距离为0
+              shape:RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24.0),
+              ),
+            ),
+            textTheme: TextTheme(
+                button: TextStyle(
+                  fontSize: Dimens.font_sp16,
+                )
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Gaps.hGap15,
+              FlatButton(
+                key: Key('goods_edit_item_$index'),
+                textColor: isDark ?  Colours.dark_button_text : Colors.white,
+                color: isDark ?  Colours.dark_app_main : Colours.app_main,
+                child: const Text('编辑'),
+                onPressed: onTapEdit,
+              ),
+              FlatButton(
+                key: Key('goods_operation_item_$index'),
+                textColor: Colours.text,
+                color: buttonColor,
+                child: const Text('下架'),
+                onPressed: onTapOperation,
+              ),
+              FlatButton(
+                key: Key('goods_delete_item_$index'),
+                textColor: Colours.text,
+                color: buttonColor,
+                child: const Text('删除'),
+                onPressed: onTapDelete,
+              ),
+              Gaps.hGap15,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _GoodsItemTag extends StatelessWidget {
+  
+  const _GoodsItemTag({
+    Key key,
+    this.color,
+    this.text,
+  }): super(key: key);
+
+  final Color color;
+  final String text;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+      margin: const EdgeInsets.only(right: 4.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(2.0),
+      ),
+      height: 16.0,
+      alignment: Alignment.center,
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: Dimens.font_sp10
+        ),
+      ),
     );
   }
 }
