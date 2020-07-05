f2ed1bdb2228de6b176100784da92b3717fa1878
weilu <a05111993@163.com>
2020-07-05 19:09:04 +0800

    #137 优化ExceptionHandle

--- lib/net/error_handle.dart
+++ lib/net/error_handle.dart
@@ -13,38 +13,49 @@ class ExceptionHandle {
   static const int parse_error = 1001;
   static const int socket_error = 1002;
   static const int http_error = 1003;
-  static const int timeout_error = 1004;
-  static const int cancel_error = 1005;
+  static const int connect_timeout_error = 1004;
+  static const int send_timeout_error = 1005;
+  static const int receive_timeout_error = 1006;
+  static const int cancel_error = 1007;
   static const int unknown_error = 9999;
+
+  static Map<int, NetError> _errorMap = <int, NetError>{
+    net_error             :   NetError(net_error, '网络异常，请检查你的网络！'),
+    parse_error           :   NetError(parse_error, '数据解析错误！'),
+    socket_error          :   NetError(socket_error, '网络异常，请检查你的网络！'),
+    http_error            :   NetError(http_error, '服务器异常，请稍后重试！'),
+    connect_timeout_error :   NetError(connect_timeout_error, '连接超时！'),
+    send_timeout_error    :   NetError(send_timeout_error, '请求超时！'),
+    receive_timeout_error :   NetError(receive_timeout_error, '响应超时！'),
+    cancel_error          :   NetError(cancel_error, '取消请求'),
+    unknown_error         :   NetError(unknown_error, '未知异常'),
+  };
 
   static NetError handleException(dynamic error) {
     print(error);
     if (error is DioError) {
-      if (error.type == DioErrorType.DEFAULT || 
-          error.type == DioErrorType.RESPONSE) {
-        dynamic e = error.error;
-        if (e is SocketException) {
-          return NetError(socket_error, '网络异常，请检查你的网络！');
-        }
-        if (e is HttpException) {
-          return NetError(http_error, '服务器异常！');
-        }
-        if (e is FormatException) {
-          return NetError(parse_error, '数据解析错误！');
-        }
-        return NetError(net_error, '网络异常，请检查你的网络！');
-      } else if (error.type == DioErrorType.CONNECT_TIMEOUT ||
-          error.type == DioErrorType.SEND_TIMEOUT ||
-          error.type == DioErrorType.RECEIVE_TIMEOUT) {
-        return NetError(timeout_error, '连接超时！');
-      } else if (error.type == DioErrorType.CANCEL) {
-        return NetError(cancel_error, '取消请求');
+      if (error.type.errorCode == 0) {
+        return _handleException(error.error);
       } else {
-        return NetError(unknown_error, '未知异常');
+        return _errorMap[error.type.errorCode];
       }
     } else {
-      return NetError(unknown_error, '未知异常');
+      return _handleException(error);
     }
+  }
+
+  static NetError _handleException(dynamic error) {
+    int errorCode = unknown_error;
+    if (error is SocketException) {
+      errorCode = socket_error;
+    }
+    if (error is HttpException) {
+      errorCode = http_error;
+    }
+    if (error is FormatException) {
+      errorCode = parse_error;
+    }
+    return _errorMap[errorCode];
   }
 }
 
@@ -53,4 +64,15 @@ class NetError{
   String msg;
 
   NetError(this.code, this.msg);
+}
+
+extension DioErrorTypeExtension on DioErrorType {
+  int get errorCode => [
+    ExceptionHandle.connect_timeout_error,
+    ExceptionHandle.send_timeout_error,
+    ExceptionHandle.receive_timeout_error,
+    0,
+    ExceptionHandle.cancel_error,
+    0,
+  ][index];
 }
