be429d1a8b1ee8b5453df794e43a0e11c32de344
weilu <a05111993@163.com>
2021-01-01 20:36:36 +0800

    引入integration_test

---
+++ integration_test/integration_test.dart
@@ -0,0 +1,5 @@
+import 'package:integration_test/integration_test_driver.dart';
+
+//flutter drive --driver integration_test/integration_test.dart --target integration_test/app_test.dart
+
+Future<void> main() => integrationDriver();
