3d32817b4ddf4189b3008ac8c8479528d66dedaa
weilu <a05111993@163.com>
2025-02-14 00:19:03 +0800

    flutter 3.29.0

--- android/app/src/main/java/com/weilu/deer/MyApp.java
+++ android/app/src/main/java/com/weilu/deer/MyApp.java
@@ -1,15 +1,14 @@
 package com.weilu.deer;
 
-import android.content.Context;
+import android.app.Application;
 
-import io.flutter.app.FlutterApplication;
 
 /**
  * @Description:
  * @Author: weilu
  * @Time: 2019/8/5 0005 17:08.
  */
-public class MyApp extends FlutterApplication {
+public class MyApp extends Application {
 
     @Override
     public void onCreate() {
