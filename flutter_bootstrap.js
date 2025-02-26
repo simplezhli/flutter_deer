c43a948083f579c52a98a1b0e41348c9096662d6
simplezhli <a05111993@163.com>
2025-02-26 14:18:36 +0000

    Deploying to gh-pages from @ simplezhli/flutter_deer@3d32817b4ddf4189b3008ac8c8479528d66dedaa 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3082609792"
+    serviceWorkerVersion: "3784001860"
   }
 });
